5d7df5bf5fe7399afc6e6bc3f17411618f1ba84d
kimbodle <rh2168@naver.com>
2025-06-03 20:10:31 +0900

    feat: color, Image Assets 추가

---
+++ sketchtalk/src/constants/colors.js
@@ -0,0 +1,14 @@
+export default {
+    primary: '#FF6947',
+    redBrown: '#463239',
+    cloudBlue: '#EBEFF6',
+    creamWhite: '#FFF9F0',
+    brown: '#403230',
+    gray100: '#F8F8F8',
+    gray200: '#DADADA',
+    gray300: '#B1B1B1',
+    gray400: '#898989',
+    blue: '#7F95D1',
+    white: '#ffffff',
+    black: '#000000',
+};
