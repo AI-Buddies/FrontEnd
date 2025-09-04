151ee600a5b36f1b8de67238dbed249c7edefe33
thekneejacks <bingepetplayer@gmail.com>
2025-09-04 20:41:44 +0900

    feat: add message row and text bar to diary screen
    네비게이션 바에서 일기화면 삭제
    안드로이드에서 키보드 펼칠 시 resize 대신 pan하도록 재설정

--- sketchtalk/src/screens/diary/DiaryMainScreen.js
+++ sketchtalk/src/screens/diary/DiaryMainScreen.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {ImageBackground, Dimensions, Text, Pressable, View} from 'react-native';
+import {
+  ImageBackground,
+  Dimensions,
+  Text,
+  Pressable,
+  View,
+  TextInput,
+} from 'react-native';
 import {styled} from 'styled-components/native';
 import colors from '../../constants/colors';
 import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
@@ -7,29 +14,54 @@ import {Image} from 'react-native';
 
 const {width, height} = Dimensions.get('window');
 
+const dummyData = [
+  {
+    key: 0,
+    isAI: false,
+    text: '오늘 친구들이랑 같이 축구를 했는데 너무 재밌었어!',
+  },
+  {
+    key: 1,
+    isAI: true,
+    text: '우 와 ~',
+  },
+  {
+    key: 2,
+    isAI: false,
+    text: '오늘 친구들이랑 같이 축구를 했는데 너무 재밌었어!',
+  },
+  {
+    key: 3,
+    isAI: true,
+    text: '오늘 친구들이랑 같이 축구를 했는데 너무 재밌었어!',
+  },
+  {
+    key: 4,
+    isAI: false,
+    text: '오늘 친구들이랑 같이 축구를 했는데 너무 재밌었어!',
+  },
+  {
+    key: 5,
+    isAI: true,
+    text: '오늘 친구들이랑 같이 축구를 했는데 너무 재밌었어!',
+  },
+];
+
 export default function DiaryMainScreen() {
   return (
     <Background
       source={require('../../assets/background/yellow_bg.png')}
       resizeMode="cover">
-      <CharacterImage/>
+      <CharacterImage />
       <MessageList
-        data={[
-          {key: '테스트'},
-          {key: '테스트'},
-          {key: '테스트'},
-          {key: '테스트'},
-          {key: '테스트'},
-          {key: '테스트'},
-          {key: '테스트'},
-          {key: '테스트'},
-          {key: '테스트'},
-          {key: '테스트'},
-        ]}
-        renderItem={({item}) => <Text>{item.key}</Text>}
+        data={dummyData}
+        contentContainerStyle={{alignItems: 'center', justifyContent: 'center'}}
+        renderItem={({item}) => MessageItem({item})}
         keyExtractor={item => item.id}
+        inverted={true}
       />
       <MicButton />
+      <TextBar />
     </Background>
   );
 }
@@ -37,9 +69,7 @@ export default function DiaryMainScreen() {
 const CharacterImage = () => (
   <View
     style={{
-      flex: 1,
-      width: 79,
-      height: 79,
+      flex: 5,
       justifyContent: 'center',
       alignItems: 'center',
     }}>
@@ -48,39 +78,132 @@ const CharacterImage = () => (
 );
 
 const MessageList = styled.FlatList`
-  flex: 1;
+  flex: 5;
+  width: ${width};
 `;
 
 const MicButton = () => (
-  <Pressable
-    style={{
-      borderRadius: Math.round(158) / 2,
-      width: 79,
-      height: 79,
-      backgroundColor: '#F6BC65',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-    onPress={() => {}}>
-    {({pressed}) => (
-      <SimpleLineIcons
-        name="microphone"
-        size={24}
-        color={pressed ? '#FF6947' : colors.white}
-      />
-    )}
-  </Pressable>
+  <View style={{flex: 1.5}}>
+    <Pressable
+      style={{
+        borderRadius: Math.round(158) / 2,
+        width: 79,
+        height: 79,
+        backgroundColor: '#F6BC65',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      onPress={() => {}}>
+      {({pressed}) => (
+        <SimpleLineIcons
+          name="microphone"
+          size={33}
+          color={pressed ? '#FF6947' : colors.white}
+        />
+      )}
+    </Pressable>
+  </View>
 );
 
-const Message = styled.Text`
-  font-size: 16px;
-  color: ${colors.primary};
-`;
+const TextBar = () => (
+  <View style={{flex: 1, width: width * 0.9}}>
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: colors.creamWhite,
+        borderRadius: 32,
+        height: 46,
+      }}>
+      <TextInput
+        style={{
+          flex: 6,
+          textAlign: 'left',
+          color: colors.black,
+          paddingLeft: 12,
+          fontSize: 16,
+          height: 46,
+          paddingBottom: 12,
+        }}
+      />
+      <Pressable
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onPress={() => alert('Yaay!')}>
+        <SimpleLineIcons name="arrow-up-circle" size={25} color="red" />
+      </Pressable>
+    </View>
+  </View>
+);
+
+function MessageItem({item}) {
+  return item.isAI ? (
+    <View
+      style={{
+        alignItems: 'flex-start',
+        width: width * 0.9,
+        marginBottom: 10,
+        paddingRight: 65,
+      }}>
+      <View
+        style={{
+          backgroundColor: colors.creamWhite,
+          paddingVertical: 10,
+          borderTopLeftRadius: 18,
+          borderTopRightRadius: 18,
+          borderBottomRightRadius: 18,
+        }}>
+        <Text
+          style={{
+            fontSize: 16,
+            textAlign: 'left',
+            paddingHorizontal: 10,
+            marginBottom: 3,
+            color: colors.black,
+          }}>
+          {item.text}
+        </Text>
+      </View>
+    </View>
+  ) : (
+    <View
+      style={{
+        alignItems: 'flex-end',
+        width: width * 0.9,
+        marginBottom: 10,
+        paddingLeft: 65,
+      }}>
+      <View
+        style={{
+          backgroundColor: colors.primary,
+          paddingVertical: 10,
+          borderTopLeftRadius: 18,
+          borderTopRightRadius: 18,
+          borderBottomLeftRadius: 18,
+        }}>
+        <Text
+          style={{
+            fontSize: 16,
+            textAlign: 'left',
+            paddingHorizontal: 10,
+            marginBottom: 3,
+            color: colors.white,
+          }}>
+          {item.text}
+        </Text>
+      </View>
+    </View>
+  );
+}
 
 const Background = styled(ImageBackground)`
   flex: 1;
-  width: ${width}px;
-  height: ${height}px;
+  width: ${width};
+  height: ${height};
   justify-content: center;
   align-items: center;
 `;
